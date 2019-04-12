3823013258cf01657eceab137ee319c687ffe2f9
VladimirGlinskikh <support@zhelezyaka.kz>
2019-04-12 15:53:55 +0600

    In class Main we make experiments with concurrency.

--- src/main/java/twoExample/Main.java
+++ src/main/java/twoExample/Main.java
@@ -5,8 +5,18 @@ import oneExample.MockLoader;
 public class Main {
     public static void main(String[] args) {
         final long before = System.currentTimeMillis();
+
         final Thread loader = new TextLoaderThread(new MockLoader("thread1"));
         loader.start();
+
+        final Runnable runnableloader = new TextLoaderThread(new MockLoader("thread2"));
+        runnableloader.run();
+
+        final Thread thread = new Thread(runnableloader);
+        thread.start();
+        final Thread thread1 = new Thread(runnableloader);
+        thread1.start();
+
         final long after = System.currentTimeMillis();
         System.out.printf("time delta: %d\n", (after - before) / 1_000);
     }
